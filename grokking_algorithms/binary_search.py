307f4afedb5b95ca6cfc5c0b8326bed10b3e5d8f
Ryan Smith <ryanjeffreysmith@gmail.com>
2022-12-19 16:22:30 -0600

    Write binary search algorithm

---
+++ grokking_algorithms/binary_search.py
@@ -0,0 +1,31 @@
+class BinarySearch():
+
+  def search_iterative(self, list, item):
+    # low and high keep track of which part of the list you'll search in.
+    low = 0
+    high = len(list) - 1
+
+    # While you haven't narrowed it down to one element...
+    while low <= high:
+      # ...check the middle element
+      mid = (low + high) // 2
+      guess = list[mid]
+      # Found the item.
+      if guess == item:
+        return mid
+      # The guess was too high.
+      if guess > item:
+        high = mid - 1
+      # The guess was too low.
+      else:
+        low = mid + 1
+
+    # Item doesn't exist
+    return None
+
+if __name__ == "__main__":
+  bs = BinarySearch()
+  my_list = [1, 3, 5, 7, 9]
+  
+  print(bs.search_iterative(my_list, 3))
+  print(bs.search_iterative(my_list, -1))
